b04e7324145a8501fb1b149f2c8285aacbd9847c
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-03-20 03:29:16 +0500

    front-page.php updated, content added.

--- front-page.php
+++ front-page.php
@@ -1,3 +1,12 @@
 <?php get_header(); ?>
 
+<div id="hero">
+    <div class="container-fluid d-flex align-items-center justify-content-center h-100">
+        <div class="content text-center">
+            <h1>Welcome to Foodiva</h1>
+            <p>A spices distributor company in usa</p>
+        </div>
+    </div>
+</div>
+
 <?php get_footer(); ?>
