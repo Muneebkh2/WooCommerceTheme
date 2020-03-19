5f3171c2b4e32320e742cd03f09ba863047d66b3
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-03-20 03:28:43 +0500

    404.php page created.

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<div id="hero">
+    <div class="container-fluid">
+        <div class="row align-items-center justify-content-center h-100">
+            <h1>404 ;) This Page Was Not Found</h1>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
