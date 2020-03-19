71e5ca0999d2534dfa3145485d112db5d6029156
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-03-19 23:57:24 +0500

    Update footer.php extra code removed.

--- footer.php
+++ footer.php
@@ -1,22 +1,4 @@
-<!-- loader -->
-<!-- <div id="ftco-loader" class="show fullscreen">
-		<svg class="circular" width="48px" height="48px">
-			<circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
-			<circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
-				stroke="#F96D00" />
-		</svg>
-	</div> -->
-
-
-	<script src="<?php echo get_template_directory_uri();?>/assets/js/jquery.min.js"></script>
-	<script src="<?php echo get_template_directory_uri();?>/assets/js/jquery-migrate-3.0.1.min.js"></script>
-	<script src="<?php echo get_template_directory_uri();?>/assets/js/popper.min.js"></script>
-	<script src="<?php echo get_template_directory_uri();?>/assets/js/bootstrap.min.js"></script>
-	<script src="<?php echo get_template_directory_uri();?>/assets/js/owl.carousel.min.js"></script>
-	<script src="<?php echo get_template_directory_uri();?>/assets/js/jquery.magnific-popup.min.js"></script>
-	<script src="<?php echo get_template_directory_uri();?>/assets/js/aos.js"></script>
-    <script src="<?php echo get_template_directory_uri();?>/assets/js/main.js"></script>
-    
+<?php wp_footer(); ?>
 </body>
 
 </html>
