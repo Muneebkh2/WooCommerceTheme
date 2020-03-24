0d17951b2cc404ab58d9817bd8f5a17131f31b7d
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-03-25 03:02:25 +0500

    Update app.js, owl-carousel added.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
 require('bootstrap');
+
+jQuery('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:true,
+    items: 4,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:5
+        }
+    }
+})
