47e0c91b5c49642ab2b4873cf0ce2259583d14e4
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-03-25 04:34:43 +0500

    Update 404.php , conatiner height added.

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div id="hero">
-    <div class="container-fluid">
+    <div class="container-fluid h-100">
         <div class="row align-items-center justify-content-center h-100">
             <h1>404 ;) This Page Was Not Found</h1>
         </div>
