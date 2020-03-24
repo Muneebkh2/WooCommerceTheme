188ed077884fd423cc1e546960faee2b3ee78f22
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-03-25 03:01:15 +0500

    Update footer.php, almost completed.

--- footer.php
+++ footer.php
@@ -1,4 +1,53 @@
-<?php wp_footer(); ?>
+<!-- footer start -->
+<footer class="main_footer">
+    <div class="container">
+        <div class="row py-5 border-bottom">
+            <div class="col-lg-3">
+                <h2 class="mb-3">Products</h2>
+                <div class="menu">
+                    <ul class="footer-nav">
+                        <li><a href="tel:+1-347-455-5097">Tandoor</a></li>
+                        <li><a href="tel:+1-347-455-5097">Masalas</a></li>
+                        <li><a href="tel:+1-347-455-5097">Dry Fruits</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2">
+                <h2 class="mb-3">Quick Links</h2>
+                <div class="menu">
+                    <ul class="footer-nav">
+                        <li><a href="tel:+1-347-455-5097">Home</a></li>
+                        <li><a href="tel:+1-347-455-5097">About us</a></li>
+                        <li><a href="tel:+1-347-455-5097">Brands</a></li>
+                        <li><a href="tel:+1-347-455-5097">Contact us</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <h2 class="mb-3">Contact Info</h2>
+                <address>
+                    <ul>
+                        <li><a href="tel:+1-347-455-5097">+1-347-455-5097</a></li>
+                        <li><a href="mailto:info@foodivausa.com">info@foodivausa.com</a></li>
+                    </ul>
+                </address>
+            </div>
+            <div class="col-lg-4">
+                <h2 class="mb-3">Payment Methods</h2>
+            </div>
+        </div>
+        <!-- copyright footer row -->
+        <div class="row py-3 footer-bottom">
+            <div class="col-lg-12 text-center">
+                <p>&copy; 2020 Foodiva Usa LLC, All Rights Reserved. Designed by <a href="https://thevirtualize.com/">Muneeb</a></p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="<?php bloginfo('template_directory'); ?>/assets/js/owl.carousel.min.js"></script>
+
+<?php wp_footer();?>
 </body>
 
 </html>
