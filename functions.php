3671ab3ab8a68858517b997e2514fe5b1b7e811a
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-03-25 03:03:51 +0500

    Update functions.php, image zoom suuport added.

--- functions.php
+++ functions.php
@@ -24,6 +24,10 @@ add_action('wp_enqueue_scripts', 'load_javascript');
 // enable options for wordpress panel
 add_theme_support('menus');
 add_theme_support('post-thumbnails');
+// Enable WooCommerce Product Gallery, Zoom & Lightbox (v3.0+)
+add_theme_support( 'wc-product-gallery-slider' );
+add_theme_support( 'wc-product-gallery-zoom' );
+add_theme_support( 'wc-product-gallery-lightbox' );
 
 
 // register_menus array
@@ -52,7 +56,67 @@ function mytheme_add_woocommerce_support() {
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
 
+// Add the cart link to menu
+function wpex_add_menu_cart_item_to_menus( $items, $args ) {
 
+	// Make sure your change 'wpex_main' to your Menu location !!!!
+	if ( $args->theme_location === 'wpex_main' ) {
+
+		$css_class = 'menu-item menu-item-type-cart menu-item-type-woocommerce-cart';
+		
+		if ( is_cart() ) {
+			$css_class .= ' current-menu-item';
+		}
+
+		$items .= '<li class="' . esc_attr( $css_class ) . '">';
+
+			$items .= wpex_menu_cart_item();
+
+		$items .= '</li>';
+
+	}
+
+	return $items;
+
+}
+add_filter( 'wp_nav_menu_items', 'wpex_add_menu_cart_item_to_menus', 10, 2 );
+
+// Function returns the main menu cart link
+function wpex_menu_cart_item() {
+
+	$output = '';
+
+	$cart_count = WC()->cart->cart_contents_count;
+
+	$css_class = 'wpex-menu-cart-total wpex-cart-total-'. intval( $cart_count );
+
+	if ( $cart_count ) {
+		$url  = WC()->cart->get_cart_url();
+	} else {
+		$url  = wc_get_page_permalink( 'shop' );
+	}
+
+	$html = $cart_extra = WC()->cart->get_cart_total();
+	$html = str_replace( 'amount', '', $html );
+
+	$output .= '<a href="'. esc_url( $url ) .'" class="' . esc_attr( $css_class ) . '">';
+
+		$output .= '<span class="fa fa-shopping-bag"></span>';
+
+		$output .= wp_kses_post( $html );
+
+	$output .= '</a>';
+
+	return $output;
+}
+
+
+// Update cart link with AJAX
+function wpex_main_menu_cart_link_fragments( $fragments ) {
+	$fragments['.wpex-menu-cart-total'] = wpex_menu_cart_item();
+	return $fragments;
+}
+add_filter( 'add_to_cart_fragments', 'wpex_main_menu_cart_link_fragments' );
 
 
 ?>
