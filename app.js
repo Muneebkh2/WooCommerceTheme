897b0e553645e841a268369560ed234f8faea95f
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-03-25 18:29:54 +0500

    Updated app.css and app.js

--- app.js
+++ app.js
@@ -17914,6 +17914,24 @@ jQuery('.owl-carousel').owlCarousel({
     }
   }
 });
+jQuery('#brandCarousel').owlCarousel({
+  margin: 10,
+  nav: false,
+  navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+  autoplay: true,
+  items: 4,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 5
+    }
+  }
+});
 
 /***/ }),
 
