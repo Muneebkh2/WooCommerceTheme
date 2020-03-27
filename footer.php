b8635c356084635712c71fb01a2af90d6542580f
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-03-27 06:42:35 +0500

    Update footer.php, links added.

--- footer.php
+++ footer.php
@@ -6,9 +6,18 @@
                 <h2 class="mb-3">Products</h2>
                 <div class="menu">
                     <ul class="footer-nav">
-                        <li><a href="tel:+1-347-455-5097">Tandoor</a></li>
-                        <li><a href="tel:+1-347-455-5097">Masalas</a></li>
-                        <li><a href="tel:+1-347-455-5097">Dry Fruits</a></li>
+                        <li><a href="<?php echo home_url(); ?>/product-category/tandoor-oven">Tandoor Oven</a></li>
+                        <li><a href="<?php echo home_url(); ?>/product-category/spices">Spices</a></li>
+                        <li><a href="<?php echo home_url(); ?>/product-category/dried-fruits-nuts">Dried-Fruits / Nuts</a></li>
+                        <li><a href="<?php echo home_url(); ?>/product-category/papads">Papads</a></li>
+                        <li><a href="<?php echo home_url(); ?>/product-category/dals-lentils">Dals / Lentils</a></li>
+                        <li><a href="<?php echo home_url(); ?>/product-category/brand-mix-spices">Brand-Mix-Spices</a></li>
+                    </ul>
+                    <ul class="footer-nav">
+                        <li><a href="<?php echo home_url(); ?>/product-category/flours">Flours</a></li>
+                        <li><a href="<?php echo home_url(); ?>/product-category/rice">Rice</a></li>
+                        <li><a href="<?php echo home_url(); ?>/product-category/chutney-pulp">Chutney / Pulp</a></li>
+                        <li><a href="<?php echo home_url(); ?>/product-category/">ACHAR / PICKEL / SAUCE</a></li>
                     </ul>
                 </div>
             </div>
@@ -16,10 +25,11 @@
                 <h2 class="mb-3">Quick Links</h2>
                 <div class="menu">
                     <ul class="footer-nav">
-                        <li><a href="tel:+1-347-455-5097">Home</a></li>
-                        <li><a href="tel:+1-347-455-5097">About us</a></li>
-                        <li><a href="tel:+1-347-455-5097">Brands</a></li>
-                        <li><a href="tel:+1-347-455-5097">Contact us</a></li>
+                        <li><a href="<?php echo home_url(); ?>">Home</a></li>
+                        <li><a href="<?php echo home_url(); ?>/about-us">About us</a></li>
+                        <li><a href="<?php echo home_url(); ?>/brands">Brands</a></li>
+                        <li><a href="<?php echo home_url(); ?>/contact-us">Contact us</a></li>
+                        <li><a href="<?php echo home_url(); ?>/terms-condition">Terms & Condition</a></li>
                     </ul>
                 </div>
             </div>
@@ -33,7 +43,12 @@
                 </address>
             </div>
             <div class="col-lg-4">
-                <h2 class="mb-3">Payment Methods</h2>
+                <h2 class="mb-3">Our Company</h2>
+                <p>
+                we are passionate on our believes that 
+                quality and affordable grocery distribution should be available for everyone.
+                </p>
+                <img src="<?php bloginfo('template_directory'); ?>/assets/images/logo.png" alt="">
             </div>
         </div>
         <!-- copyright footer row -->
