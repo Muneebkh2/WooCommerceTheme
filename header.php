ff61ac553168f66c334ec4e970d6e4648211fb6f
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-03-27 06:42:16 +0500

    Update header.php, cart-icon added.

--- header.php
+++ header.php
@@ -34,6 +34,18 @@
                             'menu_class' => 'top-menu ml-auto') );
                     ?>
                 </div>
+                <?php if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+                
+                $count = WC()->cart->cart_contents_count;
+                ?><a class="cart-contents" href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php 
+                if ( $count > 0 ) {
+                    ?>
+                    <span class="cart-contents-count"><?php echo esc_html( $count ); ?></span>
+                    <?php
+                }
+                    ?></a>
+
+                <?php } ?>
             </nav>
         </div>
     </header>
